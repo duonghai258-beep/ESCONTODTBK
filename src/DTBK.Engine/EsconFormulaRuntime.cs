0237a58b5ce546104d3f83b1f50d68116ff0f7dd
duonghai258-beep <duonghai258@gmail.com>
2026-09-08 18:19:31 +0700

    P01: preserve ESCON null ToString failure semantics

--- src/DTBK.Engine/EsconFormulaRuntime.cs
+++ src/DTBK.Engine/EsconFormulaRuntime.cs
@@ -45,7 +45,7 @@ public sealed class EsconFormulaRuntime
                 return match.Value;
 
             var value = _specialValues[key];
-            return value.ToString();
+            return value is null ? throw new NullReferenceException() : value.ToString();
         });
     }
 
